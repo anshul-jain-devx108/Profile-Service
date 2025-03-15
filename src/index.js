dbc236a595cd127e97ee54d1fa7c0c7f040db1b7
ANSHUL JAIN <anshul.jain.devx@gmail.com>
2025-03-15 23:56:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ app.use(cookieParser());
 
 app.use("/api/user", userRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.port || 3000;
 app.listen(PORT, "0.0.0.0",() => console.log(`🚀 Server running on port ${PORT}`));
